3669cdb281ac6b131f35a3c653295edebc4922b9
Igor Yurchenko <harry.urcen@gmail.com>
2018-12-18 00:03:08 +0200

    add code for implementing graceful thread completion

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,93 @@
-// use std::prelude::v1::*;
+extern crate ctrlc;
 
 use std::io;
 use std::io::prelude::*;
-
 use std::net::{TcpListener,TcpStream};
-
+//use std::process;
 use std::thread;
-// use std::sync::{Arc, Mutex};
-use std::sync::mpsc;
 use std::time::Duration;
+use std::sync::{mpsc, Arc};
+use std::sync::atomic::{AtomicBool, Ordering};
 
 mod config;
+use config::Mode;
 
-// static mut THREAD_EXEC: Arc<bool> = Arc::new(true); 
+type ExecFlag = Arc<AtomicBool>;
+type Message = String;
 
-fn run_server(ip: &str, ch: mpsc::Receiver<String>) {
+fn run_server(ip: &str, exec: ExecFlag) -> mpsc::Sender<Message> {
+    let (app_side, ch) = mpsc::channel();
+
     let listener = TcpListener::bind(&ip)
         .expect(&format!("cannot bind to ip socket {}", &ip));
 
-    thread::spawn(move || {
+    let _child = thread::spawn(move || {
         loop {
             if let Ok((stream, _)) = listener.accept() {
-                conn_handler(stream, &ch);
+                let exec = exec.clone();
+                conn_handler(stream, &ch, exec);
             }
         }
     });
+    // return (app_side, child);
+    return app_side;
 }
 
-fn run_client(ip: &str, ch: mpsc::Receiver<String>) {
+fn run_client(ip: &str, exec: ExecFlag) -> mpsc::Sender<Message> {
+    let (app_side, ch) = mpsc::channel();
     let stream = TcpStream::connect(&ip)
         .expect(&format!("cannot connect to ip {}", &ip));
 
-    thread::spawn(move || {
-                conn_handler(stream, &ch);
+    let _child = thread::spawn(move || {
+                conn_handler(stream, &ch, exec);
     });
+    // return (app_side, child);
+    return app_side;
 }
 
 fn main() {
 
     let conf = config::new();
-    match conf.mode() {
-        Mode::Usage(msg) => println!("{}", msg)
-    }
-    return;
+    let execute = Arc::new(AtomicBool::new(true));
+    let sender: mpsc::Sender<Message>;
+    // let mut proc: thread::JoinHandle<()>;
     
-    let (app_side, strm_side) = mpsc::channel();
+    match conf.mode() {
+        Mode::Server(ip_sock) => {
+            let execute = execute.clone();
+            sender = run_server(&ip_sock, execute);
+        },
+        Mode::Client(ip_sock) => {
+            let execute = execute.clone();
+            sender = run_client(&ip_sock, execute);
+        },
+        Mode::WrongConf(_) | Mode::Usage(_) => {
+            println!("{}", conf.usage());
+            return;
+        },
+    }
 
-    let _ = run_server("127.0.0.1:7878", strm_side);
-
+    // let exec = execute.clone();
+    // ctrlc::set_handler(move || {
+    //     println!("start processing handler Ctrl-C");
+    //     exec.store(false, Ordering::Relaxed);
+    //     process::exit(1);
+    // }).expect("error setting handler of ctrl-c");
+    
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let mut line = line.unwrap();
         line.push('\n');
         
-        app_side.send(line).unwrap();
+        sender.send(line).unwrap();
     }
 }
 
-fn conn_handler(mut stream: TcpStream, msgs: &mpsc::Receiver<String>) {
+fn conn_handler(mut stream: TcpStream, msgs: &mpsc::Receiver<String>, exec: ExecFlag) {
     let resp = String::from("** received\n");
     stream.set_read_timeout(Some(Duration::from_millis(500))).unwrap();
     
-    loop {
+    while exec.load(Ordering::Relaxed) {
         let mut buffer = [0; 512];
 
         if let Ok(length) = stream.read(&mut buffer) {
